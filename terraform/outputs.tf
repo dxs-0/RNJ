dc36a6e2a19e0c2df7066e3280c941e3ce78df4b
dxs-0 <dxs904@gmail.com>
2025-01-25 20:28:47 +0200

    added DNS output to TF

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,7 @@ output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.web.public_ip
 }
+
+output "instance_public_dns" {
+  value = aws_instance.web.public_dns
+}
